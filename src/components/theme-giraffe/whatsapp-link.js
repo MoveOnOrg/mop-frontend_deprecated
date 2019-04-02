3da0b20e6f9c6655a3f57e396c7c8088b9e8e3a0
shakalee14 <shakalee14@gmail.com>
2019-04-02 15:47:36 -0400

    Adds classes to correctly display or hide buttons based on device size

--- src/components/theme-giraffe/whatsapp-link.js
+++ src/components/theme-giraffe/whatsapp-link.js
@@ -5,7 +5,7 @@ import { withWhatsApp } from '../../containers/hoc-whatsapp'
 import WhatsAppSvg from 'GiraffeUI/svgs/whatsapp.svg'
 
 const WhatsAppLink = ({ onClick }) => (
-  <a className='mo-btn petition-thanks__link' onClick={onClick}>
+  <a className='mo-btn petition-thanks__link wadesktop' onClick={onClick}>
     <WhatsAppSvg />
     WhatsApp
   </a>
