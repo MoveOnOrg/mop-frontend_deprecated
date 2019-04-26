eb926ba4df505a0d5511a32d04978881f6708535
shakalee14 <shakalee14@gmail.com>
2019-04-26 12:02:07 -0400

    Changes whatsapp icon from green to blue based on cohort info

--- src/components/theme-giraffe/whatsapp-link.js
+++ src/components/theme-giraffe/whatsapp-link.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types'
 
 import { withWhatsApp } from '../../containers/hoc-whatsapp'
 import WhatsAppSvg from 'GiraffeUI/svgs/whatsapp.svg'
+import WhatsAppGreenSvg from 'GiraffeUI/svgs/whatsappgreen.svg'
 import { addBrandedColorClass } from '../../lib'
 
 const WhatsAppLink = ({ onClick, cohort }) => (
   <a className={`mo-btn petition-thanks__link d-none d-lg-inline-flex ${addBrandedColorClass('whatsapp', cohort)}`} onClick={onClick}>
-    <WhatsAppSvg />
+    {cohort ? <WhatsAppGreenSvg /> : <WhatsAppSvg />}
     WhatsApp
   </a>
 )
